90aff6297782e2c2ce0a7f6c78b785761e7d2e12
Al Ganiev <helios.ag@gmail.com>
2022-01-29 17:49:04 +1000

    Added validation

--- tgstat/words.go
+++ tgstat/words.go
@@ -3,28 +3,34 @@ package tgstat
 import (
 	"context"
 	"encoding/json"
-	"fmt"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"net/http"
 	"tgstat/endpoints"
 	"tgstat/schema"
 )
 
 type MentionPeriodRequest struct {
-	Q string
-	PeerType *string
-	StartDate *string
-	EndDate *string
-	HideForwards *bool
-	StrongSearch *bool
-	MinusWords *string
-	Group *string
+	Q              string
+	PeerType       *string
+	StartDate      *string
+	EndDate        *string
+	HideForwards   *bool
+	StrongSearch   *bool
+	MinusWords     *string
+	Group          *string
 	ExtendedSyntax *bool
+}
+
+func (mentionPeriodRequest MentionPeriodRequest) Validate() error {
+	return validation.ValidateStruct(&mentionPeriodRequest,
+		validation.Field(&mentionPeriodRequest.Q, validation.Required),
+	)
 }
 
 func (c *Client) ChannelMentionsByPeriod(ctx context.Context, request MentionPeriodRequest) (*schema.WordsMentionsResponse, *http.Response, error) {
 	path := endpoints.WordsMentionsByPeriod
 
-	if err := validateMentionsByPeriod(request); err != nil {
+	if err := request.Validate(); err != nil {
 		return nil, nil, err
 	}
 
@@ -33,11 +39,29 @@ func (c *Client) ChannelMentionsByPeriod(ctx context.Context, request MentionPer
 	body["peerType"] = *request.PeerType
 	body["startDate"] = *request.StartDate
 	body["EndDate"] = *request.EndDate
-	body["hideForwards"] = func() string { if *request.HideForwards == true { return "1" } else { return "0" } }()
-	body["strongSearch"] = func() string { if *request.StrongSearch == true { return "1" } else { return "0" } }()
+	body["hideForwards"] = func() string {
+		if *request.HideForwards == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
+	body["strongSearch"] = func() string {
+		if *request.StrongSearch == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
 	body["minusWords"] = *request.MinusWords
 	body["group"] = *request.Group
-	body["extendedSyntax"] = func() string { if *request.ExtendedSyntax == true { return "1" } else { return "0" } }()
+	body["extendedSyntax"] = func() string {
+		if *request.ExtendedSyntax == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
 	req, err := c.NewRestRequest(ctx, "GET", path, body)
 
 	if err != nil {
@@ -54,28 +78,27 @@ func (c *Client) ChannelMentionsByPeriod(ctx context.Context, request MentionPer
 	return &response, result, err
 }
 
-func validateMentionsByPeriod(request MentionPeriodRequest) error {
-	if request.Q == "" {
-		return fmt.Errorf("q must be set")
-	}
-	return nil
+type MentionsByChannelRequest struct {
+	Q              string
+	PeerType       *string
+	StartDate      *string
+	EndDate        *string
+	HideForwards   *bool
+	StrongSearch   *bool
+	MinusWords     *string
+	ExtendedSyntax *bool
 }
 
-type MentionsByChannelRequest struct {
-	Q string
-	PeerType *string
-	StartDate *string
-	EndDate *string
-	HideForwards *bool
-	StrongSearch *bool
-	MinusWords *string
-	ExtendedSyntax *bool
+func (mentionsByChannelRequest MentionsByChannelRequest) Validate() error {
+	return validation.ValidateStruct(&mentionsByChannelRequest,
+		validation.Field(&mentionsByChannelRequest.Q, validation.Required),
+	)
 }
 
 func (c *Client) WordsMentionsByChannels(ctx context.Context, request MentionsByChannelRequest) (*schema.WordsMentionsResponse, *http.Response, error) {
 	path := endpoints.WordsMentionsByChannels
 
-	if err := validateMentionsByChannelsRequest(request); err != nil {
+	if err := request.Validate(); err != nil {
 		return nil, nil, err
 	}
 
@@ -84,10 +107,28 @@ func (c *Client) WordsMentionsByChannels(ctx context.Context, request MentionsBy
 	body["peerType"] = *request.PeerType
 	body["startDate"] = *request.StartDate
 	body["EndDate"] = *request.EndDate
-	body["hideForwards"] = func() string { if *request.HideForwards == true { return "1" } else { return "0" } }()
-	body["strongSearch"] = func() string { if *request.StrongSearch == true { return "1" } else { return "0" } }()
+	body["hideForwards"] = func() string {
+		if *request.HideForwards == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
+	body["strongSearch"] = func() string {
+		if *request.StrongSearch == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
 	body["minusWords"] = *request.MinusWords
-	body["extendedSyntax"] = func() string { if *request.ExtendedSyntax == true { return "1" } else { return "0" } }()
+	body["extendedSyntax"] = func() string {
+		if *request.ExtendedSyntax == true {
+			return "1"
+		} else {
+			return "0"
+		}
+	}()
 
 	req, err := c.NewRestRequest(ctx, "GET", path, body)
 
@@ -103,13 +144,4 @@ func (c *Client) WordsMentionsByChannels(ctx context.Context, request MentionsBy
 	_ = json.NewDecoder(result.Body).Decode(&response)
 
 	return &response, result, err
-}
-
-func validateMentionsByChannelsRequest(request MentionsByChannelRequest) error {
-
-	if request.Q == "" {
-		return fmt.Errorf("q must be set")
-	}
-
-	return nil
 }
