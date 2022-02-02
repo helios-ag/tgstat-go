d8738fef7331955457100debfd346b80396ce71e
Al Ganiev <helios.ag@gmail.com>
2022-02-03 00:34:31 +1000

    enumerate all endpoints

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -2,26 +2,28 @@ package endpoints
 
 // API endpoints
 const (
-	ChannelsGet         string = "/channels/get"
-	ChannelsSearch      string = "/channels/search"
-	ChannelsStat        string = "/channels/stat"
-	ChannelsMentions    string  = "/channels/mentions"
-	ChannelsForwards    string = "/channels/forwards"
-	ChannelsSubscribers string = "/channels/subscribers"
-	ChannelsViews       string = "/channels/views"
-	ChannelsAdd         string = "/channels/add"
+	ChannelsGet          string = "/channels/get"
+	ChannelsSearch       string = "/channels/search"
+	ChannelsStat         string = "/channels/stat"
+	ChannelsMentions     string = "/channels/mentions"
+	ChannelsForwards     string = "/channels/forwards"
+	ChannelsSubscribers  string = "/channels/subscribers"
+	ChannelsViews        string = "/channels/views"
+	ChannelAVGPostsReach string = "/channels/avg-posts-reach"
+	ChannelErr           string = "/channels/err"
 
-	PostsGet string = "/posts/get"
-	PostsStat string = "/posts/stat"
+	ChannelsAdd string = "/channels/add"
+
+	PostsGet    string = "/posts/get"
+	PostsStat   string = "/posts/stat"
 	PostsSearch string = "/posts/search"
 
-	WordsMentionsByPeriod string = "/words/mentions-by-period"
+	WordsMentionsByPeriod   string = "/words/mentions-by-period"
 	WordsMentionsByChannels string = "/words/mentions-by-channels"
 
 	UsageStat string = "/usage/stat"
 
 	DatabaseCategories string = "/database/categories"
-	DatabaseCountries string = "/database/countries"
-	DatabaseLanguages string = "/database/languages"
+	DatabaseCountries  string = "/database/countries"
+	DatabaseLanguages  string = "/database/languages"
 )
-
