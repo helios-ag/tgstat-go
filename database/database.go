865847f364daa47e4694d45f94c8066c0a742bb6
Al Ganiev <helios.ag@gmail.com>
2022-02-15 17:00:01 +1000

    dictinaries fix

--- database/database.go
+++ database/database.go
@@ -1,28 +1,36 @@
-package tgstat
+package database
 
 import (
 	"context"
 	"encoding/json"
+	tgstat "github.com/helios-ag/tgstat-go"
+	"github.com/helios-ag/tgstat-go/endpoints"
+	"github.com/helios-ag/tgstat-go/schema"
 	"net/http"
-	"tgstat/endpoints"
-	//"github.com/helios-ag/tgstat-go/endpoints"
-	//"github.com/helios-ag/tgstat-go/schema"
-	"tgstat/schema"
 )
 
-func (c *Client) CountriesGet(ctx context.Context, lang string) (*schema.CountryResponse, *http.Response, error) {
+type Client struct {
+	API   tgstat.API
+	Token string
+}
+
+func CountriesGet(ctx context.Context, lang string) (*schema.CountryResponse, *http.Response, error) {
+	return getClient().CountriesGet(ctx, lang)
+}
+
+func (c Client) CountriesGet(ctx context.Context, lang string) (*schema.CountryResponse, *http.Response, error) {
 	path := endpoints.DatabaseCountries
 
 	body := make(map[string]string)
 	body["lang"] = lang
-	req, err := c.NewRestRequest(ctx, "GET", path, body)
+	req, err := c.API.NewRestRequest(ctx, "GET", path, body)
 
 	if err != nil {
 		return nil, nil, err
 	}
 
 	var response schema.CountryResponse
-	result, err := c.Do(req, &response)
+	result, err := c.API.Do(req, &response)
 	if err != nil {
 		return nil, result, err
 	}
@@ -31,20 +39,24 @@ func (c *Client) CountriesGet(ctx context.Context, lang string) (*schema.Country
 	return &response, result, err
 }
 
-func (c *Client) CategoriesGet(ctx context.Context, lang string) (*schema.CategoryResponse, *http.Response, error) {
+func CategoriesGet(ctx context.Context, lang string) (*schema.CategoryResponse, *http.Response, error) {
+	return getClient().CategoriesGet(ctx, lang)
+}
+
+func (c Client) CategoriesGet(ctx context.Context, lang string) (*schema.CategoryResponse, *http.Response, error) {
 	path := endpoints.DatabaseCategories
 
 	body := make(map[string]string)
 	body["lang"] = lang
 
-	req, err := c.NewRestRequest(ctx, "GET", path, body)
+	req, err := c.API.NewRestRequest(ctx, "GET", path, body)
 
 	if err != nil {
 		return nil, nil, err
 	}
 
 	var response schema.CategoryResponse
-	result, err := c.Do(req, &response)
+	result, err := c.API.Do(req, &response)
 	if err != nil {
 		return nil, result, err
 	}
@@ -53,23 +65,31 @@ func (c *Client) CategoriesGet(ctx context.Context, lang string) (*schema.Catego
 	return &response, result, err
 }
 
-func (c *Client) LanguagesGet(ctx context.Context, lang string) (*schema.LanguageResponse, *http.Response, error) {
+func LanguagesGet(ctx context.Context, lang string) (*schema.LanguageResponse, *http.Response, error) {
+	return getClient().LanguagesGet(ctx, lang)
+}
+
+func (c Client) LanguagesGet(ctx context.Context, lang string) (*schema.LanguageResponse, *http.Response, error) {
 	path := endpoints.DatabaseLanguages
 
 	body := make(map[string]string)
 	body["lang"] = lang
-	req, err := c.NewRestRequest(ctx, "GET", path, body)
+	req, err := c.API.NewRestRequest(ctx, "GET", path, body)
 
 	if err != nil {
 		return nil, nil, err
 	}
 
 	var response schema.LanguageResponse
-	result, err := c.Do(req, &response)
+	result, err := c.API.Do(req, &response)
 	if err != nil {
 		return nil, result, err
 	}
 	_ = json.NewDecoder(result.Body).Decode(&response)
 
 	return &response, result, err
+}
+
+func getClient() Client {
+	return Client{tgstat.GetAPI(), tgstat.Token}
 }
