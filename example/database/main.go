a505e423c9beb2fde123fdd03a4ba57e3afaeeec
Al Ganiev <helios.ag@gmail.com>
2022-03-01 22:26:47 +1000

    db example

---
+++ example/database/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	tgstat "github.com/helios-ag/tgstat-go"
+	"github.com/helios-ag/tgstat-go/database"
+	"os"
+)
+
+func getToken() (key string, err error) {
+	key = os.Getenv("TOKEN")
+	if "" == key {
+		return "", fmt.Errorf("token not found")
+	}
+
+	return key, nil
+}
+
+func main() {
+	token, err := getToken()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Token is %s\n", token)
+
+	tgstat.Token = token
+	languages, _, err := database.LanguagesGet(context.Background(), "ru")
+	categories, _, err := database.CategoriesGet(context.Background(), "ru")
+	countries, _, err := database.CountriesGet(context.Background(), "ru")
+
+	if err != nil {
+		fmt.Printf("error getting data: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, info := range languages.Response {
+		fmt.Printf("Name: %s\n", info.Name)
+		fmt.Printf("Code: %s\n", info.Code)
+	}
+	fmt.Print("Categories list")
+	for _, info := range categories.Response {
+		fmt.Printf("Name: %s\n", info.Name)
+		fmt.Printf("Code: %s\n", info.Code)
+	}
+	fmt.Print("Countries list")
+	for _, info := range countries.Response {
+		fmt.Printf("Name: %s\n", info.Name)
+		fmt.Printf("Code: %s\n", info.Code)
+	}
+
+	os.Exit(0)
+}
