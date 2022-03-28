51895f2bcc2bba23c80be41d320519e346f243d4
Al Ganiev <helios.ag@gmail.com>
2022-03-28 11:11:04 +1000

    more coverage

--- schema/words.go
+++ schema/words.go
@@ -1,34 +1,44 @@
 package schema
 
+type WordsMentionsItem struct {
+	Period        string `json:"period"`
+	MentionsCount int    `json:"mentions_count"`
+	ViewsCount    int    `json:"views_count"`
+}
+
 type WordsMentionsResponse struct {
-	Status   string `json:"status"`
-	Response struct {
-		Items []struct {
-			Period        string `json:"period"`
-			MentionsCount int    `json:"mentions_count"`
-			ViewsCount    int    `json:"views_count"`
-		} `json:"items"`
-	} `json:"response"`
+	Items []WordsMentionsItem `json:"items"`
+}
+
+type WordsMentions struct {
+	Status   string                `json:"status"`
+	Response WordsMentionsResponse `json:"response"`
+}
+
+type WordsMentionsByChannelItem struct {
+	ChannelID       int `json:"channel_id"`
+	MentionsCount   int `json:"mentions_count"`
+	ViewsCount      int `json:"views_count"`
+	LastMentionDate int `json:"last_mention_date"`
+}
+
+type WordsMentionsByChannelChannel struct {
+	ID                int    `json:"id"`
+	Link              string `json:"link"`
+	Username          string `json:"username"`
+	Title             string `json:"title"`
+	About             string `json:"about"`
+	Image100          string `json:"image100"`
+	Image640          string `json:"image640"`
+	ParticipantsCount int    `json:"participants_count"`
 }
 
 type WordsMentionsByChannelResponse struct {
-	Status   string `json:"status"`
-	Response struct {
-		Items []struct {
-			ChannelID       int `json:"channel_id"`
-			MentionsCount   int `json:"mentions_count"`
-			ViewsCount      int `json:"views_count"`
-			LastMentionDate int `json:"last_mention_date"`
-		} `json:"items"`
-		Channels []struct {
-			ID                int    `json:"id"`
-			Link              string `json:"link"`
-			Username          string `json:"username"`
-			Title             string `json:"title"`
-			About             string `json:"about"`
-			Image100          string `json:"image100"`
-			Image640          string `json:"image640"`
-			ParticipantsCount int    `json:"participants_count"`
-		} `json:"channels"`
-	} `json:"response"`
+	Items    []WordsMentionsByChannelItem    `json:"items"`
+	Channels []WordsMentionsByChannelChannel `json:"channels"`
+}
+
+type WordsMentionsByChannel struct {
+	Status   string                         `json:"status"`
+	Response WordsMentionsByChannelResponse `json:"response"`
 }
--- words/client_test.go
+++ words/client_test.go
@@ -17,20 +17,53 @@ func prepareClient(URL string) {
 	tgstat.WithEndpoint(URL)
 }
 
-func TestClient_ChannelMentionsByPeriod(t *testing.T) {
+func makeStrP(s string) *string {
+	return &s
+}
+
+func makeBoolP(b bool) *bool {
+	return &b
+}
+
+func TestClient_MentionsByPeriod(t *testing.T) {
 	RegisterTestingT(t)
-	t.Run("Test mention order validation", func(t *testing.T) {
+	t.Run("Test words request validation", func(t *testing.T) {
 		prepareClient("localhost")
 		req := MentionPeriodRequest{
-			Q:              "",
-			PeerType:       nil,
-			StartDate:      nil,
-			EndDate:        nil,
-			HideForwards:   nil,
-			StrongSearch:   nil,
-			MinusWords:     nil,
-			Group:          nil,
-			ExtendedSyntax: nil,
+			Q: "",
+		}
+		_, _, err := MentionsByPeriod(context.Background(), req)
+		Expect(err).To(HaveOccurred())
+		Expect(err.Error()).To(ContainSubstring("Q: cannot be blank"))
+	})
+
+	t.Run("Test words request validation", func(t *testing.T) {
+		testServer := server.NewServer()
+		defer testServer.Teardown()
+		prepareClient(testServer.URL)
+
+		testServer.Mux.HandleFunc(endpoints.WordsMentionsByPeriod, func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			items := make([]schema.WordsMentionsItem, 0)
+			items = append(items, schema.WordsMentionsItem{
+				Period:        "2018-11-04",
+				MentionsCount: 1000,
+				ViewsCount:    3985,
+			})
+
+			response := schema.WordsMentionsResponse{
+				Items: items,
+			}
+			json.NewEncoder(w).Encode(schema.WordsMentions{
+				Status:   "ok",
+				Response: response,
+			})
+		})
+
+		req := MentionPeriodRequest{
+			Q:        "",
+			PeerType: makeStrP("5"),
 		}
 		_, _, err := MentionsByPeriod(context.Background(), req)
 		Expect(err).To(HaveOccurred())
@@ -45,17 +78,19 @@ func TestClient_ChannelMentionsByPeriod(t *testing.T) {
 		testServer.Mux.HandleFunc(endpoints.WordsMentionsByPeriod, func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(schema.WordsMentionsResponse{
-				Status: "ok",
-				Response: struct {
-					Items []struct {
-						Period        string `json:"period"`
-						MentionsCount int    `json:"mentions_count"`
-						ViewsCount    int    `json:"views_count"`
-					} `json:"items"`
-				}{
-					Items: nil,
-				},
+			items := make([]schema.WordsMentionsItem, 0)
+			items = append(items, schema.WordsMentionsItem{
+				Period:        "2018-11-04",
+				MentionsCount: 1000,
+				ViewsCount:    3985,
+			})
+
+			response := schema.WordsMentionsResponse{
+				Items: items,
+			}
+			json.NewEncoder(w).Encode(schema.WordsMentions{
+				Status:   "ok",
+				Response: response,
 			})
 		})
 
@@ -66,22 +101,83 @@ func TestClient_ChannelMentionsByPeriod(t *testing.T) {
 		_, _, err := MentionsByPeriod(context.Background(), req)
 		Expect(err).ToNot(HaveOccurred())
 	})
-}
 
-func TestClient_WordsMentionsByPeriod(t *testing.T) {
-	RegisterTestingT(t)
 	t.Run("Test mention order validation", func(t *testing.T) {
 		prepareClient("http://nonexistinghost")
 
 		req := MentionPeriodRequest{
-			Q: "q",
+			Q:        "q",
+			PeerType: makeStrP("p"),
 		}
 		_, _, err := MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:         "q",
+			PeerType:  makeStrP("p"),
+			StartDate: makeStrP("2020-01-19 03:14:07"),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:            "q",
+			PeerType:     makeStrP("p"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:            "q",
+			PeerType:     makeStrP("p"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(false),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:            "q",
+			PeerType:     makeStrP("p"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(false),
+			MinusWords:   makeStrP("something"),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:            "q",
+			PeerType:     makeStrP("p"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(true),
+			MinusWords:   makeStrP("something"),
+			Group:        makeStrP("hope"),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
+		req = MentionPeriodRequest{
+			Q:              "q",
+			PeerType:       makeStrP("p"),
+			EndDate:        makeStrP("2020-01-19 03:14:07"),
+			HideForwards:   makeBoolP(true),
+			StrongSearch:   makeBoolP(true),
+			MinusWords:     makeStrP("something"),
+			Group:          makeStrP("hope"),
+			ExtendedSyntax: makeBoolP(true),
+		}
+		_, _, err = MentionsByPeriod(context.Background(), req)
+
 		Expect(err).To(HaveOccurred())
 		Expect(err.Error()).To(ContainSubstring("dial tcp"))
 	})
 
-	t.Run("Test mention order response Mapping", func(t *testing.T) {
+}
+
+func TestClient_MentionsByChannels(t *testing.T) {
+	RegisterTestingT(t)
+
+	t.Run("Test mention by channels order response Mapping", func(t *testing.T) {
 		testServer := server.NewServer()
 		defer testServer.Teardown()
 		prepareClient(testServer.URL)
@@ -89,7 +185,7 @@ func TestClient_WordsMentionsByPeriod(t *testing.T) {
 		testServer.Mux.HandleFunc(endpoints.WordsMentionsByChannels, func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(schema.WordsMentionsResponse{
+			json.NewEncoder(w).Encode(schema.WordsMentions{
 				Status: "ok",
 			})
 		})
@@ -99,6 +195,72 @@ func TestClient_WordsMentionsByPeriod(t *testing.T) {
 		}
 
 		_, _, err := MentionsByChannels(context.Background(), req)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	t.Run("Test mention order response Mapping", func(t *testing.T) {
+		prepareClient("http://nonexisting")
+
+		req := MentionsByChannelRequest{
+			Q:        "q",
+			PeerType: makeStrP("p"),
+		}
+		_, _, err := MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:         "q",
+			PeerType:  makeStrP("chat"),
+			StartDate: makeStrP("2020-01-19 03:14:07"),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:            "q",
+			PeerType:     makeStrP("all"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:            "q",
+			PeerType:     makeStrP("all"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(false),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:            "q",
+			PeerType:     makeStrP("all"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(false),
+			MinusWords:   makeStrP("something"),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:            "q",
+			PeerType:     makeStrP("chat"),
+			EndDate:      makeStrP("2020-01-19 03:14:07"),
+			HideForwards: makeBoolP(true),
+			StrongSearch: makeBoolP(true),
+			MinusWords:   makeStrP("something"),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
+
+		req = MentionsByChannelRequest{
+			Q:              "q",
+			PeerType:       makeStrP("chat"),
+			EndDate:        makeStrP("2020-01-19 03:14:07"),
+			HideForwards:   makeBoolP(true),
+			StrongSearch:   makeBoolP(true),
+			MinusWords:     makeStrP("something"),
+			ExtendedSyntax: makeBoolP(true),
+		}
+		_, _, err = MentionsByChannels(context.Background(), req)
 		Expect(err).ToNot(HaveOccurred())
 	})
 }
