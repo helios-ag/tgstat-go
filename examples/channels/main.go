a36c044a40e535415a0813d7e78c0f0a5786ec86
Al Ganiev <helios.ag@gmail.com>
2022-05-14 12:39:14 +1000

    statickcheck

--- examples/channels/main.go
+++ examples/channels/main.go
@@ -41,6 +41,21 @@ func main() {
 	fmt.Printf("Username: %s\n", channelInfo.Response.Username)
 	fmt.Printf("Title: %s\n", channelInfo.Response.Title)
 	fmt.Printf("About: %s\n", channelInfo.Response.About)
+	fmt.Printf("Category: %s\n", channelInfo.Response.Category)
+	fmt.Printf("Country: %s\n", channelInfo.Response.Country)
+	fmt.Printf("Language: %s\n", channelInfo.Response.Language)
+	fmt.Printf("Image100: %s\n", channelInfo.Response.Image100)
+	fmt.Printf("Image640: %s\n", channelInfo.Response.Image640)
+	fmt.Printf("ParticipantsCount: %d\n", channelInfo.Response.ParticipantsCount)
+	fmt.Printf("RedLabel: %s\n", bool2string(channelInfo.Response.TGStatRestriction.RedLabel))
+	fmt.Printf("BlackLabel: %s\n", bool2string(channelInfo.Response.TGStatRestriction.BlackLabel))
 
 	os.Exit(0)
+}
+
+func bool2string(b bool) string {
+	if b {
+		return "yes"
+	}
+	return "no"
 }
